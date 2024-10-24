287140b9510c81559b61bb4d9495b81ef8f701c4
Tauseef Shaikh <tauseefsh@gmail.com>
2024-10-24 15:46:10 +0530

    updated add new items.js file to handle non duplicate ID

--- additems.js
+++ additems.js
@@ -1,8 +1,13 @@
 export let inventory = [];
 
 export const addNewItems = (id, name, quantity, price) => {
+    if (inventory.find(item => item.id === id)) {
+        console.log(`Item with ID: ${id} already exists`);
+        return;
+    } else {
     const item = { id, name, quantity, price };
     inventory.push(item);
     console.log(`Added ${item.name} to the inventory`);
+    }
 };
 // module.exports = addNewItems;
